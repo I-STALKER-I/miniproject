edf182305d7cddb4230bb67284cdec90ebaed172
STALKER <sina.golshahi@gmail.com>
2023-04-06 01:12:34 +0430

    merged the feature

--- mdmn.py
+++ mdmn.py
@@ -1,18 +1,5 @@
 import datetime
 
-<<<<<<< HEAD
-class person :
-    _users = []
-    def __init__(self,id) :
-        self.id = id
-        self.last_cancel = None
-
-    def reservation_cancelation(self,date_time) :
-        self.last_cancel = date_time
-class ticket :
-    _tickets = []
-    def __init__(self,date_time ,user_id ,ticket_dapar ,ticket_arrival) :
-=======
 
 class person :
     _users = []
@@ -52,38 +39,17 @@ class ticket :
     _tickets = []
     staged_for_remove_vips = {}
     def __init__(self,date_time ,user_id ,ticket_dapar ,ticket_arrival ,level = None,order = None) :
->>>>>>> feature
         self.date_time = date_time
         self.user_id = user_id
         self.departure = ticket_dapar
         self.arrival = ticket_arrival
-<<<<<<< HEAD
-=======
         self.order = order
         self.level = level
->>>>>>> feature
 
     @property
     def date_time(self) :
         return self._date_time
     
-<<<<<<< HEAD
-    @date_time.setter
-    def date_time(self,ticket_date_time) :
-        """the ticket_date_time we get is a string we have to separate the year and month and minute and  etc..."""
-
-        first_slash_index = ticket_date_time.find('/')
-        year = int(ticket_date_time[:first_slash_index])
-        month = int(ticket_date_time[first_slash_index + 1:first_slash_index + 3])
-        day = int(ticket_date_time[first_slash_index + 4:first_slash_index + 6])
-        hour = int(ticket_date_time[first_slash_index + 7 : first_slash_index + 9])
-        minute = int(ticket_date_time[first_slash_index + 10 : first_slash_index + 12])
-
-        self._date_time = datetime.datetime(year ,month ,day ,hour ,minute)
-    
-    def __eq__(self,other_ticket) :
-        if self.departure == other_ticket.departure and self.arrival == other_ticket.arrival and self.user_id == other_ticket.user_id :
-=======
     @property
     def level(self) :
         return self._level
@@ -121,22 +87,10 @@ class ticket :
     def __eq__(self,other_ticket) :
         if self.departure == other_ticket.departure and self.arrival == other_ticket.arrival and self.user_id == other_ticket.user_id and self.level == other_ticket.level :
 
->>>>>>> feature
             return True
         
         return False
     
-<<<<<<< HEAD
-
-class train :
-    _trains = []
-
-    def __init__(self ,train_depar ,train_arri ,train_date_time ,train_sits) :
-        self.departure = train_depar
-        self.arrival = train_arri
-        self.date_time = train_date_time
-        self.sits = int(train_sits)
-=======
     @classmethod
     def removation(cls,_ticket) :
         if _ticket.level ==  0 :
@@ -159,7 +113,6 @@ class train :
         self.arrival = train_arri
         self.date_time = train_date_time
         self.sits = {0 : int(train_normal_sits)}
->>>>>>> feature
 
     @property
     def date_time(self) :
@@ -178,17 +131,6 @@ class train :
 
         self._date_time = datetime.datetime(year ,month ,day ,hour ,minute)
     
-<<<<<<< HEAD
-    def sit_reservation(self) :
-        if self.sits > 0 :
-            self.sits -= 1
-            return True
-        
-        return False
-    
-    def sit_cancelation(self) :
-        self.sits += 1
-=======
     def sit_reservation(self,ticket_level,user_id,date_time) :
         if ticket_level == None :
             ticket_level = 0
@@ -225,7 +167,6 @@ class train :
     def sit_cancelation(self,sit_class) :
         
         self.sits[sit_class] += 1
->>>>>>> feature
 
     def __eq__(self,ticket) :
         if self.departure == ticket.departure and self.arrival == ticket.arrival :
@@ -235,8 +176,6 @@ class train :
         return False
 
 
-<<<<<<< HEAD
-=======
 class Order(ticket) :
     _orders = []
     def __init__(self ,date_time ,user_id ,ticket_departure ,ticket_arrival ,level = None ,order = None) :
@@ -275,7 +214,6 @@ class Order(ticket) :
     that in it we had overrided __eq__ once.this causes problem in our hashing so we have to override our __eq__ again """
 
 
->>>>>>> feature
 try : 
     train_cnt = int(input())
 
@@ -285,10 +223,6 @@ except ValueError :
 
 for i in range(train_cnt) :
         try :
-<<<<<<< HEAD
-            train_departure ,train_arrival ,train_date_time ,train_sits = map(str ,input().split())
-            new_train = train(train_departure ,train_arrival ,train_date_time ,train_sits)
-=======
             train_departure ,train_arrival ,train_date_time ,train_normal_sits,*train_vip_sits = map(str ,input().split())
 
             new_train = train(train_departure ,train_arrival ,train_date_time ,train_normal_sits)
@@ -298,7 +232,6 @@ for i in range(train_cnt) :
                 for i ,j in enumerate(vip_count) :
                     new_train.sits[i + 1] = j
 
->>>>>>> feature
             train._trains.append(new_train)
 
         except ValueError :
@@ -313,64 +246,6 @@ except ValueError :
     print("Error")
     exit()
 
-<<<<<<< HEAD
-def order_manager(date_time,ticket_user_id,ticket_departure,ticket_destination,order = None) :
-
-    new_ticket = ticket(date_time,ticket_user_id,ticket_departure,ticket_destination)
-
-    if order == None :
-        for _train in train._trains :
-            if _train == new_ticket :
-                if _train.sit_reservation() : 
-                    ticket._tickets.append(new_ticket)
-                    person._users.append(person(ticket_user_id))
-                    return True
-
-        return False
-
-    elif order == "cancel" :
-
-        for _ticket in ticket._tickets :
-            if new_ticket == _ticket :
-                for _train in train._trains :
-                    if _train == _ticket :
-                        for user in person._users :
-                            if user.id == ticket_user_id :
-                                if user.last_cancel == None :
-                                    user.reservation_cancelation(new_ticket.date_time)
-                                    ticket._tickets.remove(_ticket)
-                                    _train.sit_cancelation()
-                                    return True
-                                
-                                else :
-                                    if new_ticket.date_time - user.last_cancel >= datetime.timedelta(0,3600) :
-                                        user.reservation_cancelation(new_ticket.date_time)
-                                        ticket._tickets.remove(_ticket)
-                                        _train.sit_cancelation()
-                                        return True
-                            
-                                    else :
-                                        return False
-                        return False
-                    
-                return False
-
-        return False
-
-    else :
-        raise ValueError
-
-for i in range(calls_cnt) :
-        try :
-            call_order = input().split()
-            if order_manager(*call_order) :
-                print("Movafagh")
-            else :
-                print("Na Movafagh")
-
-        except ValueError :
-            print("Error")
-=======
 def order_manager(order_instance,date_time,ticket_user_id,ticket_departure,ticket_destination,order = None,order_2 = None) :
 
     new_ticket = ticket(date_time ,ticket_user_id ,ticket_departure ,ticket_destination ,order ,order_2)
@@ -457,4 +332,3 @@ for _order in Order._orders :
 
 for order in not_sorted_list :
     print(order.answer)
->>>>>>> feature
